0b930dcf0aada6cc7a28604972a80469bebdf04e
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-04-19 17:38:02 +0200

    no longer standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactCompiler: true,
-  cacheComponents: true
-};
+  cacheComponents: true,
+  typedRoutes: true,
+}
 
-export default nextConfig;
+export default nextConfig
